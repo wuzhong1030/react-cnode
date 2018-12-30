1e1725e7ee7b4b97703462a06f443f8589de62d8
wuzhong1598@qq.com <wuzhong1598@qq.com>
2018-12-30 21:10:07 +0800

    add user login

--- client/store/app-state.js
+++ client/store/app-state.js
@@ -1,27 +1,27 @@
 import { observable, computed, action } from "mobx";
+import { post } from "../utils/request";
 
 export default class AppState {
-  constructor({ count, name } = { count: 0, name: "Stone" }) {
-    this.count = count;
-    this.name = name;
-  }
-  @observable count;
-  @observable name;
-  @computed get msg() {
-    return `${this.name} say count is ${this.count}`;
-  }
-  @action add() {
-    this.count += 1;
-  }
-  @action changeName(name) {
-    this.name = name;
-  }
-  // 用于服务端渲染，把改变后的数据传给客户端页面
-  toJson() {
-    return {
-      count: this.count,
-      name: this.name
-    };
+  @observable user = {
+    isLogin: false,
+    info: {}
+  };
+
+  @action login(accessToken) {
+    return new Promise((resolve, reject) => {
+      post("/api/user/login", {
+        accessToken
+      })
+        .then(res => {
+          if (res.success) {
+            this.user.isLogin = true;
+            this.user.info = res.data;
+            resolve();
+          } else {
+            reject(res);
+          }
+        })
+        .catch(reject);
+    });
   }
 }
-
---
+++ client/views/user/login.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { inject, observer } from "mobx-react";
+
+import { TextField, Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+import UserWrapper from "./user";
+import loginStyle from "./style/login-style";
+
+@inject(store => {
+  return {
+    appState: store.appState,
+    user: store.appState.user
+  };
+})
+@observer
+class UserLogin extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      accessToken: "",
+      helpText: ""
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.user.isLogin) {
+      this.context.router.history.replace("/user/info");
+    } else {
+    }
+  }
+
+  handleInput(e) {
+    this.setState({
+      accessToken: e.target.value.trim()
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <UserWrapper>
+        <div className={classes.root}>
+          <TextField
+            label="accessToken"
+            placeholder="请输入accessToken"
+            required
+            helperText={this.state.helpText}
+            value={this.state.accessToken}
+            onChange={this.handleInput}
+            className={classes.input}
+          />
+          <Button
+            color="accent"
+            onClick={this.handleLogin}
+            className={classes.loginButton}
+          >
+            登陆
+          </Button>
+        </div>
+      </UserWrapper>
+    );
+  }
+}
+
+UserLogin.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+UserLogin.wrappedComponent.propTypes = {
+  appState: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+export default withStyles(loginStyle)(UserLogin);
