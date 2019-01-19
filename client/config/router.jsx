1e31c9bd50ac1e33b44853ebcc0990d2fb3ff07c
wuzhong1598@qq.com <wuzhong1598@qq.com>
2019-01-19 17:07:05 +0800

    add code

--- client/config/router.jsx
+++ client/config/router.jsx
@@ -8,6 +8,41 @@ import TopicCreate from '../views/topic-create/index';
 
 import Test from '../../client/test/api-test';
 
+import { inject, observer } from 'mobx-react';
+import { PropTypes } from 'prop-types';
+
+const PriviateRoute = ({ isLogin, component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isLogin ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/user/login',
+            search: `?from=${rest.path}`,
+          }}
+        />
+      )
+    }
+  />
+);
+
+const InjectPriviateRoute = inject(store => {
+  return {
+    isLogin: store.appState.user.isLogin,
+  };
+})(observer(PriviateRoute));
+
+PriviateRoute.propTypes = {
+  isLogin: PropTypes.bool,
+  component: PropTypes.element.isRequired,
+};
+PriviateRoute.defaultProps = {
+  isLogin: false,
+};
+
 export default () => [
   <Route
     exact
@@ -16,9 +51,16 @@ export default () => [
     key="redirect"
   />,
   <Route path="/list" component={TopicList} key="list" />,
-  <Route path="/create" component={TopicCreate} key="create" />,
+
+  <InjectPriviateRoute path="/create" component={TopicCreate} key="create" />,
+
   <Route exact path="/detail/:id" component={TopicDetail} key="detail" />,
   <Route exact path="/user/login" component={UserLogin} key="login" />,
-  <Route exact path="/user/info" component={UserInfo} key="info" />,
+  <InjectPriviateRoute
+    exact
+    path="/user/info"
+    component={UserInfo}
+    key="info"
+  />,
   <Route exact path="/test" component={Test} key="test" />,
 ];
